96d2b4552e891a5b2ad2c1ecd03f69be204bc8dc
StinaAxelsson <stina91@live.se>
2022-03-17 09:22:26 +0000

    Install django and create superuser

---
+++ crystalized/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for crystalized project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crystalized.settings')
+
+application = get_wsgi_application()
